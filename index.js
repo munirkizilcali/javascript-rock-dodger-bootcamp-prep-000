864af86affb984483b6f59360bcbd3307e767787
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-19 22:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -104,9 +104,8 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-     if (rock.parentNode === GAME) {
+
       GAME.removeChild(rock)
-     }
      }
   }
 
